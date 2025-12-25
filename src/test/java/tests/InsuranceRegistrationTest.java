4107a7093c42ff673adb45eecf3da5c83508c33b
Jonmiz <jonmiz12@gmail.com>
2025-12-25 21:28:05 +0200

    test

--- src/test/java/tests/InsuranceRegistrationTest.java
+++ src/test/java/tests/InsuranceRegistrationTest.java
@@ -1,12 +1,19 @@
 package tests;
+//import io.qameta.allure.
+import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 
 public class InsuranceRegistrationTest extends BaseTest{
     @Test
+    @Description("This test validates the registration of insurance policies")
     public void tc01_Register(){
         TravelPolicyPage.waitForLoad().clickFirstTimePurchase();
         DestinationPage.waitForLoad().clickAsia().clickMoveToDatePicker();
         DatePickerPage.waitForLoad().clickDateByDays(7).clickDateByDays(37).clickNextBtn();
         assert (PassengersPage.isPassengersPageOpened());
+    }
+
+    public void step1(){
+
     }
 }
